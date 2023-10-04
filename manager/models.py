84943966847e575952a784de906370c27c2579b7
Olexander Bryl <edlrian814@gmail.com>
2023-10-04 19:33:26 +0300

    Create models Task & TaskType & Worker & Position

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,45 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        "Position", on_delete=models.CASCADE, related_name="workers"
+    )
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name",]
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name", ]
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=255, choices=[
+        ("Ur", "Urgent"), ("H", "High"), ("M", "Medium"), ("L", "Low"),
+    ])
+    task_type = models.ForeignKey(
+        "TaskType", on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["-deadline"]
+
+    def __str__(self) -> str:
+        return f"{self.name} (deadline:{self.deadline})"
